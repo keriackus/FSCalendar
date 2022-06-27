9864e30e9061086862c058bb239de9e7b0e3ba7c
Keriackus <abanoub.keriackus@outlook.com>
2022-06-27 11:04:55 +0200

    Update FSCalendarWeekdayView.h
    Expose hiding showing notifications on each weekday

--- FSCalendar/FSCalendarWeekdayView.h
+++ FSCalendar/FSCalendarWeekdayView.h
@@ -22,6 +22,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)configureAppearance;
 
+- (void)showNotificationLabel:(int) index withCount:(int)count;
+- (void)hideNotificationLabel:(int) index;
+
 @end
 
 NS_ASSUME_NONNULL_END
